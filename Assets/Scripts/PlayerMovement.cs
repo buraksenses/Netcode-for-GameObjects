8081fa1c769ffa3afe5cd5c9661191d14dbe6bec
Burak Şenses <buraksenseees@gmail.com>
2022-10-27 16:30:42 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,16 +11,16 @@ namespace Sirhot.Player
     {
         private Transform _playerTransform;
         private Rigidbody _playerRb;
-        private PlayerInputActions playerInputActions;
+        private PlayerInputActions _playerInputActions;
 
         private void Awake()
         {
             _playerTransform = transform;
             _playerRb = GetComponent<Rigidbody>();
 
-            playerInputActions = new PlayerInputActions();
-            playerInputActions.Player.Enable();
-            playerInputActions.Player.Jump.performed += Jump;
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.Enable();
+            _playerInputActions.Player.Jump.performed += Jump;
         }
 
         private void Start()
@@ -37,7 +37,7 @@ namespace Sirhot.Player
 
         private void Move()
         {
-            Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+            Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
             float speed = 5f;
             _playerTransform.position += new Vector3(inputVector.x, 0, inputVector.y) * (speed * Time.deltaTime);
         }
